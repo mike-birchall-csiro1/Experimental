47f278cb094e9b9affe13c483957eea8f1481f07
Mike Birchall <mike.birchall@csiro.au>
2025-01-21 14:08:33 +1100

    Added new without delete for the CodeQL to pick up

--- CodeQL/main.cc
+++ CodeQL/main.cc
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+class badClass {
+	int codeno;
+	bool flag;
+};
+
+void test() {
+	char* c=new char[0];
+	*c='K';
+	cout<<c<<endl;
+	//delete(c);
+
+}
+
 int main(int argc,char** argv) {
 
 	cout << " CodeQL Test " << endl;
-	
+	test();	
 	return 0;
 	
 }
